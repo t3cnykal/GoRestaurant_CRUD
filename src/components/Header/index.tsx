d08418efcb0f7766300298ae9ff42be59a43a921
t3cnykal <brunocosta_r@hotmail.com>
2020-06-01 13:39:18 +0100

    CRUD Update

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -3,8 +3,6 @@ import React from 'react';
 import { FiPlusSquare } from 'react-icons/fi';
 import { Container } from './styles';
 
-import Logo from '../../assets/logo.svg';
-
 interface IHeaderProps {
   openModal: () => void;
 }
@@ -12,13 +10,13 @@ interface IHeaderProps {
 const Header: React.FC<IHeaderProps> = ({ openModal }) => (
   <Container>
     <header>
-      <img src={Logo} alt="GoRestaurant" />
       <nav>
         <div>
           <button
             type="button"
             onClick={() => {
               /* TODO OPEN MODAL */
+              openModal();
             }}
           >
             <div className="text">Novo Prato</div>
--- src/components/ModalEditFood/index.tsx
+++ src/components/ModalEditFood/index.tsx
@@ -2,6 +2,7 @@ import React, { useRef, useCallback } from 'react';
 
 import { FiCheckSquare } from 'react-icons/fi';
 import { FormHandles } from '@unform/core';
+
 import { Form } from './styles';
 import Modal from '../Modal';
 import Input from '../Input';
@@ -40,6 +41,8 @@ const ModalEditFood: React.FC<IModalProps> = ({
   const handleSubmit = useCallback(
     async (data: IEditFoodData) => {
       // EDIT A FOOD PLATE AND CLOSE THE MODAL
+      handleUpdateFood(data);
+      setIsOpen();
     },
     [handleUpdateFood, setIsOpen],
   );
